b8f1b9248ccc682fb74cb38a3faad661122bf903
Mark Smith <smith.h.mark@gmail.com>
2023-03-05 16:51:21 -0700

    passing tests for stub subscribe

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -41,6 +41,37 @@ async fn subscribe_returns_200_for_valid_form() {
     assert_eq!(200, response.status().as_u16());
 }
 
+#[tokio::test]
+async fn subscribe_returns_400_for_missing_form_data() {
+    //Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "Missing Email"),
+        ("email=ursula_le_quin%40example.com", "Missing Name"),
+        ("", "Missing Both"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        //Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        //Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 as required for {}",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
     let port = listener.local_addr().unwrap().port();
