54b89ef68ebe06c5271ae573f7b6dae40a677ee6
Mark Smith <smith.h.mark@gmail.com>
2023-03-05 16:14:56 -0700

    new failing test for subscriptions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -18,6 +18,29 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+//Arrange
+//Act
+//Assert
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form() {
+    //Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    //Act
+    let body = "name=le%20guin&email=ursula_le_quin%40example.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    //Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
     let port = listener.local_addr().unwrap().port();
