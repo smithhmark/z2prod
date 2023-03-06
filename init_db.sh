d05afa58cecceda65357cd7a7b4b6407f196c511
Mark Smith <smith.h.mark@gmail.com>
2023-03-05 22:16:53 -0700

    beginning DB setup

---
+++ init_db.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+#set -x
+#set -eo pipefail
+
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD=${POSTGRES_PASSWORD:=password}
+DB_NAME=${POSTGRES_DB:=newsletter}
+DB_PORT=${POSTGRES_PORT:=5432}
+
+read -p "Press Enter to continue" </dev/tty
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres:15 postgres -N 1000
+
+read -p "Press Enter to continue" </dev/tty
+
+DB_HOST=${POSTGRES_HOST:=localhost}
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+
